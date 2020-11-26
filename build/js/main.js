e68b85bcca989d0e3869d40bfb130697dcc808cc
Rin <rin.anemos@gmail.com>
2020-11-26 15:29:28 +0300

    added validation form

--- build/js/main.js
+++ build/js/main.js
@@ -1,17 +1,56 @@
 
+/* 
 
-$(document).ready(function() {
- 
-// Здравствуйте!
-// Вы здесь ожидали увидеть валидацию, но ее здесь пока нет) Она появится завтра днем. 
-// Потому что я с такой задачей раньше не сталкивалась и хочу не просто код из интернета воткнуть, но разобраться и сделать хорошо.
+Добрый день)
+Как я и обещала, добавила валидацию формы. 
 
-// Извините за задержку.
+Что бы вы ни решили по итогу проверки, буду благодарна за feedback.
 
+*/
+
+
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	checkIputs();
 });
 
+function checkIputs(){
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
 
+	if(emailValue === ''){ 
+			setErrorFor(email, 'Email cannot be blank');
 
+	} else if(!isEmail(emailValue)){
+
+			setErrorFor(email, 'Email is not valid');
+
+	}else{
+		setSuccessFor(email);
+	}
+}
+
+function setErrorFor(input, message){
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+
+	small.innerText = message;
+	formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input){
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
 
 		
 
